b93a431ea7ca46438b9e3838ad6d8bc1ad3fe337
Haichen Shen <haichen@cs.washington.edu>
2015-04-18 13:55:43 -0700

    add iplane vp info

---
+++ gen_vp_info.rb
@@ -0,0 +1,33 @@
+require 'set'
+
+require_relative 'config.rb'
+require_relative 'retrieve_data.rb'
+require_relative 'lib/asmapper.rb'
+
+include TopoConfig
+
+if $0  == __FILE__
+    startdate = Date.parse('20150219')
+    duration = 7
+
+    numday = 0
+    vplist = Set.new
+    output = "iplane_vps.txt"
+    fout = File.open(output, 'w')
+
+    while numday < duration
+        date = (startdate + numday).strftime("%Y%m%d")
+        numday += 1
+        tracelist = retrieve_iplane(date)
+        tracelist.each do |vp, _|
+            next if IPLANE_BLACKLIST.include?(vp)
+            next if vplist.include?(vp)
+
+            vp_ip = ASMapper.get_ip_from_url(vp)
+            vp_asn = ASMapper.query_asn(vp_ip)
+            fout.puts "#{vp} #{vp_ip} #{vp_asn}"
+        end
+    end
+    fout.close
+    puts "Output to #{output}"
+end
