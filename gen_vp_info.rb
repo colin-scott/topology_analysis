405af8b4ed43bd515f86c91ef215fefb0ae8e4ef
Haichen Shen <haichen@cs.washington.edu>
2015-04-27 00:39:30 -0700

    update vps info

--- gen_vp_info.rb
+++ gen_vp_info.rb
@@ -12,19 +12,19 @@ if $0  == __FILE__
 
     numday = 0
     vplist = Set.new
-    #output = "data/iplane_vps.txt"
-    output = "data/yahoo_vps.txt"
+    output = "data/iplane_vps.txt"
+    #output = "data/yahoo_vps.txt"
     fout = File.open(output, 'w')
 
     while numday < duration
         date = (startdate + numday).strftime("%Y%m%d")
         numday += 1
-        #tracelist = retrieve_iplane(date)
-        tracelist = retrieve_yahoo(date)
+        tracelist = retrieve_iplane(date)
+        #tracelist = retrieve_yahoo(date)
         tracelist.each do |vp, _|
-            #next if IPLANE_BLACKLIST.include?(vp)
+            next if IPLANE_BLACKLIST.include?(vp)
             next if vplist.include?(vp)
-
+            vplist << vp
             vp_ip = ASMapper.get_ip_from_url(vp)
             vp_asn = ASMapper.query_asn(vp_ip)
             fout.puts "#{vp} #{vp_ip} #{vp_asn}"
